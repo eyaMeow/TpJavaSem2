67ce1d0fcfcca20f6f4751aa462b9d6cf03be193
eya chihi <chihiemna59@gmail.com>
2025-02-27 21:42:04 +0100

    finished tp7ex3

--- TP7/tp7ex3.java
+++ TP7/tp7ex3.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 
 public class tp7ex3 {
     public static void main(String[] args) {
-        int[][] m=new int[20][30];
-        int[] t=new int[30];
-        Scanner scanner=new Scanner()
-        System.out.println();
+        int[][] m = new int[20][30];
+        int[] t = new int[20];
+        int nl, nc;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("donner nl");
+            nl = sc.nextInt();
+        } while (nl > 20 || nl < 0);
+        do {
+            System.out.println("donner nc");
+            nc = sc.nextInt();
+        } while (nc > 30 || nc < 0);
+        for (int i = 0; i < nl; i++) {
+            for (int j = 0; j < nc; j++) {
+                System.out.println("donner M[" + i + "," + j + "]");
+                m[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < nl; i++) {
+            int somme = 0;
+            for (int j = 0; j < nc; j++) {
+                somme += m[i][j];
+            }
+            t[i] = somme;
+        }
+        for (int i = 0; i < nl; i++) {
+            System.out.println(t[i]);
+        }
     }
 }
